d956d948ac373ee7dc26bd2f871dad6cca22d772
andresuriza <urizandres@estudiantec.cr>
2021-06-04 13:12:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from tkinter import *
+# ----------------------------------------------------------------------------------------------------------------------
 # Tarea programada 2: Operation Moonlight
+# Curso: Taller de programacion
 # Programadores: Andrés Uriza Lazo y Jose Pablo Esquetini Fallas
 
-print("Prueba")
+menu_window = Tk()
+menu_window.title("Juego")
+menu_window.geometry("1500x776")
+menu_window.resizable(False, False)
+
+menu_window.mainloop()
